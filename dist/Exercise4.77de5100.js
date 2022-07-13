41ed84c196737db88681119040a7d44934b74ec9
Jose <jose.r.frias88@gmail.com>
2022-07-13 14:51:17 +0200

    Exercise4

--- dist/Exercise4.77de5100.js
+++ dist/Exercise4.77de5100.js
@@ -168,7 +168,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57175" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63726" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
