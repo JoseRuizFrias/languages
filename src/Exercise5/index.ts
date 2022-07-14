3ab69cec986eba68041ba0d83fb3aa7fe39a6af3
Jose <jose.r.frias88@gmail.com>
2022-07-14 12:23:18 +0200

    Exercise5 slot machine

---
+++ src/Exercise5/index.ts
@@ -0,0 +1,49 @@
+// SLOT MACHINE
+
+class slotMachine {
+    public coinCount: number = 0;
+
+    constructor () {
+        this.coinCount;
+    }
+
+    Play = () : void => {
+
+        const wheel1: boolean = randomBoolean();
+        const wheel2: boolean = randomBoolean();
+        const wheel3: boolean = randomBoolean();
+
+    
+        if (wheel1 === wheel2 === wheel3)
+        {
+            console.log('Congratulations!!!. You won ' + this.coinCount + ' coins!!');
+            this.coinCount = 0;
+        }
+        else
+        {
+            console.log('Good luck next time!!');
+            this.coinCount++;
+        }
+    }
+}
+
+const randomBoolean = () : boolean => Math.random() < 0.5;
+
+const machine1 = new slotMachine();
+
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
+machine1.Play()
